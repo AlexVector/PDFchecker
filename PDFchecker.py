36293c9100f371da97fe1cb4a8bcb9ad01cf2132
Alex <rucetskii.alexander@gmail.com>
2023-03-28 21:22:13 +0300

    Size of pictures is fixed now... + build and dist

--- PDFchecker.py
+++ PDFchecker.py
@@ -1,7 +1,10 @@
 import fitz
 import PyPDF2
-from PIL import Image
 from past.builtins import raw_input
+from pdf2docx import parse
+from typing import Tuple
+import zipfile
+from xml.etree import ElementTree
 import os
 
 files = os.listdir()
@@ -13,6 +16,11 @@ for file in files:
         break
 
 if pdf_file:
+    parse(file, "example.docx")
+    print('!!!ВНИМАНИЕ!!!')
+    print('Программа может не распознать некоторые графические элементы.')
+    print('Пожалуйста, сверьте количество изображений в документе с результатом программы.')
+    print('Погрешность размеров графических элементов: 1 - 3 миллиметра.')
     pdf_reader = PyPDF2.PdfReader(open(pdf_file, 'rb'))  # открываем pdf файл для чтения
     num_pages = len(pdf_reader.pages)  # получаем количество страниц в pdf файле
     total_chars = 0  # общее количество символов
@@ -27,23 +35,34 @@ if pdf_file:
         num_lines = len(text.splitlines())  # подсчитываем количество строк в тексте
         total_chars += num_chars  # добавляем к общему количеству символов
         total_lines += num_lines  # добавляем к общему количеству строк
-        page_img = doc.load_page(i) # загружаем текущую страницу
-        image_list = page_img.get_images()  # получаем список изображений
-        total_area = 0  # обнуляем суммарную площадь
-        if image_list:
-            for image in image_list:  # проходим по всем изображениям
-                xref = image[0]  # получаем ссылку на изображение
-                width = image[2]  # получаем ширину изображения в пунктах
-                height = image[3]  # получаем высоту изображения в пунктах
-                print("Изображение", xref, "размеры:", width, "x", height, "пикселей,")  # выводим результат
-                width_cm = width * 0.02635872298  # переводим ширину в сантиметры
-                height_cm = height * 0.02635872298  # переводим высоту в сантиметры
-                area_cm = width_cm * height_cm  # рассчитываем площадь в квадратных сантиметрах
-                total_area += area_cm
-                print("Изображение", xref, "размеры:", round(width_cm, 2), "x", round(height_cm, 2), "см,", "площадь:",
-                      round(area_cm, 2), "кв.см")  # выводим результат
-                print(f"Площадь графических материалов на странице {i + 1} составляет {round(total_area, 2)} кв.см.")  # выводим результат на экран
-                full_total_area += total_area;
+
+    with zipfile.ZipFile('example.docx') as docx:
+        # Extract the document.xml file from the zip file
+        with docx.open('word/document.xml') as f:
+            # Parse the XML content
+            tree = ElementTree.parse(f)
+            root = tree.getroot()
+            # Define the XML namespaces
+            ns = {
+                'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main',
+                'wp': 'http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing',
+                'a': 'http://schemas.openxmlformats.org/drawingml/2006/main',
+                'pic': 'http://schemas.openxmlformats.org/drawingml/2006/picture'
+            }
+            # Find all inline images in the document
+            img_counter = 1
+            for pic in root.findall('.//wp:inline/a:graphic/a:graphicData/pic:pic', ns):
+                # Get the image size in EMUs (English Metric Units)
+                cx = int(pic.find('pic:spPr/a:xfrm/a:ext', ns).get('cx'))
+                cy = int(pic.find('pic:spPr/a:xfrm/a:ext', ns).get('cy'))
+                # Convert the size from EMUs to centimeters (1 cm = 360000 EMUs)
+                width = cx / 360000
+                height = cy / 360000
+                this_img_area = width * height
+                full_total_area += this_img_area
+                print(f'Изображение {img_counter}: {width:.2f} x {height:.2f} см. Площадь: {this_img_area:.2f} кв. сантиметров.')
+                img_counter += 1
+    os.remove("example.docx")
 
     print(f'Количество символов: {total_chars}')
     print(f'Количество строк: {total_lines}')
@@ -51,5 +70,6 @@ if pdf_file:
     print('----------------------------------------------')
 else:
     print('Отсутствуют pdf файлы в папке программы...')
+
 print('Нажмите Enter, чтобы закрыть консоль...')
 raw_input()
